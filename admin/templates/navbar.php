3058850cbd5f9243daf73fcf11c1048b991758cc
kirtanmangukiya <96651106+kirtanmangukiya@users.noreply.github.com>
2022-04-17 22:26:38 +0530

    Add files via upload

---
+++ admin/templates/navbar.php
@@ -0,0 +1,28 @@
+ <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+  <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="index.php">A1 Store</a>
+
+    	<?php
+    		if (isset($_SESSION['admin_id'])) {
+    			?>
+    				<a class="nav-link" href="../admin/admin-logout.php">Sign out</a>
+    			<?php
+    		}else{
+    			$uriAr = explode("/", $_SERVER['REQUEST_URI']);
+    			$page = end($uriAr);
+    			if ($page === "login.php") {
+    				
+    			}else{
+    				?>
+	    				<a class="nav-link" href="../admin/login.php">Login</a>
+	    			<?php
+    			}
+
+
+    			
+    		}
+
+    	?>
+      
+    </li>
+  </ul>
+</nav>
